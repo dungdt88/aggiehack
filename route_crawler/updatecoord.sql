082a69d95a97968e0094128d56f318b6468ca285
cookievan <vantrinh81@gmail.com>
2013-10-12 15:56:28 -0500

    Update file updatecoord.sql

--- route_crawler/updatecoord.sql
+++ route_crawler/updatecoord.sql
@@ -3,36 +3,36 @@ UPDATE location SET latitude=30.62095300, longitude=-96.33822100 WHERE id=2; /* 
 UPDATE location SET latitude=30.62722200, longitude=-96.34027000 WHERE id=3; /* Community Center */
 UPDATE location SET latitude=30.62068500, longitude=-96.33827400 WHERE id=4; /* WERC */
 UPDATE location SET latitude=30.60943900, longitude=-96.35344500 WHERE id=5; /* Rural Public Health */
-
-	(6,'Munson',-96.32160200,30.63281600),
-	(7,'Carter Creek Shopping Center',-96.33333900,30.64670000),
-	(8,'Blinn College - Bldg E',-96.35157800,30.66339600),
-	(9,'Willow Oaks',-96.33368300,30.64660700),
-	(10,'Reveille Ranch',-96.35960300,30.62864400),
-	(11,'Aggie Station',-96.35256500,30.62469200),
-	(12,'Trigon',-96.33926200,30.61355700),
-	(13,'Campus View',-96.31726700,30.61994700),
-	(14,'Plantation Oaks',-96.30665700,30.62746200),
-	(15,'The Cambridge',-96.31718200,30.62026000),
-	(16,'Brentwood',-96.31364100,30.60637300),
-	(17,'Trails @ Wolf Pen Creek',-96.30044500,30.61911600),
-	(18,'Redstone',-96.31284700,30.60794300),
-	(19,'Taos',-96.32529300,30.61263400),
-	(20,'Mall Park - n - Ride',-96.30469300,30.62063000),
-	(21,'HEB',-96.31823300,30.61206100),
-	(22,'Scandia',-96.32355500,30.61136000),
-	(23,'Oney Hervey',-96.33099000,30.59780400),
-	(24,'University Park ',-96.32115100,30.59754500),
-	(25,'Parkway Circle',-96.32685900,30.59333400),
-	(26,'Doux Chene/Valley View',-96.30709600,30.59787800),
-	(27,'Peppertree',-96.29545600,30.59359200),
-	(28,'Ponderosa @ Brothers Pond Park',-96.29960800,30.58300800),
-	(29,'Westridge @ Antelope',-96.31846900,30.58280500),
-	(30,'Welsh @ San Benito',-96.30953200,30.58618500),
-	(31,'Fraternity Row #2',-96.31445600,30.57615400),
-	(32,'Treehouse',-96.33928300,30.60245800),
-	(33,'Jones Butler South',-96.33537800,30.59087700),
-	(34,'The Heights',-96.34679300,30.58969500),
-	(35,'Meadows Point 1',-96.33660100,30.59601200),
-	(36,'Holleman South',-96.33699800,30.58408900),
-	(37,'U-Club',-96.33653600,30.59625200);
+UPDATE location SET latitude=30.63281600, longitude=-96.32160200 WHERE id=6; /* Munson */
+UPDATE location SET latitude=30.64670000, longitude=-96.33333900 WHERE id=7; /* Carter Creek Shopping Center */
+UPDATE location SET latitude=30.66339600, longitude=-96.35157800 WHERE id=8; /* Blinn College - Bldg E */
+UPDATE location SET latitude=30.64660700, longitude=-96.33368300 WHERE id=9; /* Willow Oaks */
+UPDATE location SET latitude=30.62864400, longitude=-96.35960300 WHERE id=10; /* Reveille Ranch */
+UPDATE location SET latitude=30.62469200, longitude=-96.35256500 WHERE id=11; /* Aggie Station */
+UPDATE location SET latitude=30.61355700, longitude=-96.33926200 WHERE id=12; /* Trigon */
+UPDATE location SET latitude=30.61994700, longitude=-96.31726700 WHERE id=13; /* Campus View */
+UPDATE location SET latitude=30.62746200, longitude=-96.30665700 WHERE id=14; /* Plantation Oaks */
+UPDATE location SET latitude=30.62026000, longitude=-96.31718200 WHERE id=15; /* The Cambridge */
+UPDATE location SET latitude=30.60637300, longitude=-96.31364100 WHERE id=16; /* Brentwood */
+UPDATE location SET latitude=30.61911600, longitude=-96.30044500 WHERE id=17; /* Trails @ Wolf Pen Creek */
+UPDATE location SET latitude=30.60794300, longitude=-96.31284700 WHERE id=18; /* Redstone */
+UPDATE location SET latitude=30.61263400, longitude=-96.32529300 WHERE id=19; /* Taos */
+UPDATE location SET latitude=30.62063000, longitude=-96.30469300 WHERE id=20; /* Mall Park - n - Ride */
+UPDATE location SET latitude=30.61206100, longitude=-96.31823300 WHERE id=21; /* HEB */
+UPDATE location SET latitude=30.61136000, longitude=-96.32355500 WHERE id=22; /* Scandia */
+UPDATE location SET latitude=30.59780400, longitude=-96.33099000 WHERE id=23; /* Oney Hervey */
+UPDATE location SET latitude=30.59754500, longitude=-96.32115100 WHERE id=24; /* University Park */
+UPDATE location SET latitude=30.59333400, longitude=-96.32685900 WHERE id=25; /* Parkway Circle */
+UPDATE location SET latitude=30.59787800, longitude=-96.30709600 WHERE id=26; /* Doux Chene/Valley View */
+UPDATE location SET latitude=30.59359200, longitude=-96.29545600 WHERE id=27; /* Peppertree */
+UPDATE location SET latitude=30.58300800, longitude=-96.29960800 WHERE id=28; /* Ponderosa @ Brothers Pond Park */
+UPDATE location SET latitude=30.58280500, longitude=-96.31846900 WHERE id=29; /* Westridge @ Antelope */
+UPDATE location SET latitude=30.58618500, longitude=-96.30953200 WHERE id=30; /* Welsh @ San Benito */
+UPDATE location SET latitude=30.57615400, longitude=-96.31445600 WHERE id=31; /* Fraternity Row #2 */
+UPDATE location SET latitude=30.60245800, longitude=-96.33928300 WHERE id=32; /* Treehouse */
+UPDATE location SET latitude=30.59087700, longitude=-96.33537800 WHERE id=33; /* Jones Butler South */	
+UPDATE location SET latitude=30.58969500, longitude=-96.34679300 WHERE id=34; /* The Heights */
+UPDATE location SET latitude=30.59601200, longitude=-96.33660100 WHERE id=35; /* Meadows Point 1 */
+UPDATE location SET latitude=30.58408900, longitude=-96.33699800 WHERE id=36; /* Holleman South */	
+UPDATE location SET latitude=30.59625200, longitude=-96.33653600 WHERE id=1; /* U-Club */	
+	
