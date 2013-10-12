7b69c4d3cabf3aa6adbac409175004eca86caac4
Dung Dam <dungdt@smartosc.com>
2013-10-12 12:59:31 -0500

    updated bus route finder

--- web_app/src/Visa/DemoBundle/Controller/DefaultController.php
+++ web_app/src/Visa/DemoBundle/Controller/DefaultController.php
@@ -3,11 +3,22 @@
 namespace Visa\DemoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Class DefaultController
+ * @package Visa\DemoBundle\Controller
+ */
 class DefaultController extends Controller
 {
+    /**
+     * @return Response
+     */
     public function indexAction()
     {
         return $this->render('VisaDemoBundle:Default:index.html.twig');
     }
+
+    
 }
