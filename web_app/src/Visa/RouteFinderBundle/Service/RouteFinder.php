7d30ee0d279f900861f6bf554cf4c82af4a0aa60
Dung Dam <dungdt@smartosc.com>
2013-10-12 18:14:17 -0500

    updated

--- web_app/src/Visa/RouteFinderBundle/Service/RouteFinder.php
+++ web_app/src/Visa/RouteFinderBundle/Service/RouteFinder.php
@@ -38,51 +38,59 @@ class RouteFinder
     {
         $startTime = date('H:i:s', strtotime($startTime));
         
-        $handle = popen(sprintf('python %s -a %s -b %s -c %s -d %s -s %s',
-            $this->calculatorPath . '/RouteCalculator.py',
-            $fromLatitude,
-            $fromLongitude,
-            $toLatitude,
-            $toLongitude,
-            $startTime
-            ), 'r');
-        $result = fread($handle, 100);
-        $result = trim($result, " \n\r\t");
+//        $handle = popen(sprintf('python %s -a %s -b %s -c %s -d %s -s %s',
+//            $this->calculatorPath . '/RouteCalculator.py',
+//            $fromLatitude,
+//            $fromLongitude,
+//            $toLatitude,
+//            $toLongitude,
+//            $startTime
+//            ), 'r');
+//        var_dump(sprintf('python %s -a %s -b %s -c %s -d %s -s %s',
+//            $this->calculatorPath . '/RouteCalculator.py',
+//            $fromLatitude,
+//            $fromLongitude,
+//            $toLatitude,
+//            $toLongitude,
+//            $startTime));
+//        die;
+//        $result = fread($handle, 100);
+//        $result = trim($result, " \n\r\t");
 
-        $routes = $this->formatResult($result);
+//        $routes = $this->formatResult($result);
 //        
-//        $routes = array(
-//            array(
-//                'from' => '109 Maple ave College Station',
-//                'to' => 'Zachry Building',
-//                'time' => '20:00:00',
-//                'type' => 'Walk',
-//            ),
-//            array(
-//                'from' => '109 Maple ave College Station',
-//                'to' => 'Zachry Building',
-//                'time' => '20:00:00',
-//                'type' => 'Walk',
-//            ),
-//            array(
-//                'from' => '109 Maple ave College Station',
-//                'to' => 'Zachry Building',
-//                'time' => '20:00:00',
-//                'type' => 'Walk',
-//            ),
-//            array(
-//                'from' => '109 Maple ave College Station',
-//                'to' => 'Zachry Building',
-//                'time' => '20:00:00',
-//                'type' => 'Walk',
-//            ),
-//            array(
-//                'from' => '109 Maple ave College Station',
-//                'to' => 'Zachry Building',
-//                'time' => '20:00:00',
-//                'type' => 'Walk',
-//            ),
-//        );
+        $routes = array(
+            array(
+                'from' => '109 Maple ave College Station',
+                'to' => 'Zachry Building',
+                'time' => '20:00:00',
+                'type' => 'Walk',
+            ),
+            array(
+                'from' => '109 Maple ave College Station',
+                'to' => 'Zachry Building',
+                'time' => '20:00:00',
+                'type' => 'Walk',
+            ),
+            array(
+                'from' => '109 Maple ave College Station',
+                'to' => 'Zachry Building',
+                'time' => '20:00:00',
+                'type' => 'Walk',
+            ),
+            array(
+                'from' => '109 Maple ave College Station',
+                'to' => 'Zachry Building',
+                'time' => '20:00:00',
+                'type' => 'Walk',
+            ),
+            array(
+                'from' => '109 Maple ave College Station',
+                'to' => 'Zachry Building',
+                'time' => '20:00:00',
+                'type' => 'Walk',
+            ),
+        );
         
         return $routes;
     }
