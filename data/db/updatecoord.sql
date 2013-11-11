41d3a90dbd09265e5473f913a3c6a525ffc11423
cookievan <vantrinh81@gmail.com>
2013-11-10 18:11:12 -0600

    Rearrange data folder.

---
+++ data/db/updatecoord.sql
@@ -0,0 +1,38 @@
+UPDATE location SET latitude=30.61393756, longitude=-96.33965217 WHERE id=1; /* MSC */
+UPDATE location SET latitude=30.62095300, longitude=-96.33822100 WHERE id=2; /* Wisenbaker */
+UPDATE location SET latitude=30.62722200, longitude=-96.34027000 WHERE id=3; /* Community Center */
+UPDATE location SET latitude=30.62068500, longitude=-96.33827400 WHERE id=4; /* WERC */
+UPDATE location SET latitude=30.60943900, longitude=-96.35344500 WHERE id=5; /* Rural Public Health */
+UPDATE location SET latitude=30.63281600, longitude=-96.32160200 WHERE id=6; /* Munson */
+UPDATE location SET latitude=30.64670000, longitude=-96.33333900 WHERE id=7; /* Carter Creek Shopping Center */
+UPDATE location SET latitude=30.66339600, longitude=-96.35157800 WHERE id=8; /* Blinn College - Bldg E */
+UPDATE location SET latitude=30.64660700, longitude=-96.33368300 WHERE id=9; /* Willow Oaks */
+UPDATE location SET latitude=30.62864400, longitude=-96.35960300 WHERE id=10; /* Reveille Ranch */
+UPDATE location SET latitude=30.62469200, longitude=-96.35256500 WHERE id=11; /* Aggie Station */
+UPDATE location SET latitude=30.61355700, longitude=-96.33926200 WHERE id=12; /* Trigon */
+UPDATE location SET latitude=30.61994700, longitude=-96.31726700 WHERE id=13; /* Campus View */
+UPDATE location SET latitude=30.62746200, longitude=-96.30665700 WHERE id=14; /* Plantation Oaks */
+UPDATE location SET latitude=30.62026000, longitude=-96.31718200 WHERE id=15; /* The Cambridge */
+UPDATE location SET latitude=30.60637300, longitude=-96.31364100 WHERE id=16; /* Brentwood */
+UPDATE location SET latitude=30.61911600, longitude=-96.30044500 WHERE id=17; /* Trails @ Wolf Pen Creek */
+UPDATE location SET latitude=30.60794300, longitude=-96.31284700 WHERE id=18; /* Redstone */
+UPDATE location SET latitude=30.61263400, longitude=-96.32529300 WHERE id=19; /* Taos */
+UPDATE location SET latitude=30.62063000, longitude=-96.30469300 WHERE id=20; /* Mall Park - n - Ride */
+UPDATE location SET latitude=30.61206100, longitude=-96.31823300 WHERE id=21; /* HEB */
+UPDATE location SET latitude=30.61136000, longitude=-96.32355500 WHERE id=22; /* Scandia */
+UPDATE location SET latitude=30.59780400, longitude=-96.33099000 WHERE id=23; /* Oney Hervey */
+UPDATE location SET latitude=30.59754500, longitude=-96.32115100 WHERE id=24; /* University Park */
+UPDATE location SET latitude=30.59333400, longitude=-96.32685900 WHERE id=25; /* Parkway Circle */
+UPDATE location SET latitude=30.59787800, longitude=-96.30709600 WHERE id=26; /* Doux Chene/Valley View */
+UPDATE location SET latitude=30.59359200, longitude=-96.29545600 WHERE id=27; /* Peppertree */
+UPDATE location SET latitude=30.58300800, longitude=-96.29960800 WHERE id=28; /* Ponderosa @ Brothers Pond Park */
+UPDATE location SET latitude=30.58280500, longitude=-96.31846900 WHERE id=29; /* Westridge @ Antelope */
+UPDATE location SET latitude=30.58618500, longitude=-96.30953200 WHERE id=30; /* Welsh @ San Benito */
+UPDATE location SET latitude=30.57615400, longitude=-96.31445600 WHERE id=31; /* Fraternity Row #2 */
+UPDATE location SET latitude=30.60245800, longitude=-96.33928300 WHERE id=32; /* Treehouse */
+UPDATE location SET latitude=30.59087700, longitude=-96.33537800 WHERE id=33; /* Jones Butler South */	
+UPDATE location SET latitude=30.58969500, longitude=-96.34679300 WHERE id=34; /* The Heights */
+UPDATE location SET latitude=30.59601200, longitude=-96.33660100 WHERE id=35; /* Meadows Point 1 */
+UPDATE location SET latitude=30.58408900, longitude=-96.33699800 WHERE id=36; /* Holleman South */	
+UPDATE location SET latitude=30.59625200, longitude=-96.33653600 WHERE id=1; /* U-Club */	
+	
