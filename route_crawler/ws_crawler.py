e9127d86f8f4897d7a3ca3ef28a4c44ff815d91b
cookievan <vantrinh81@gmail.com>
2013-11-22 11:50:08 -0600

    Fix web service crawler to save all info into one json file. Fix to use Timer instead of wait.

--- route_crawler/ws_crawler.py
+++ route_crawler/ws_crawler.py
@@ -6,7 +6,7 @@ import sys
 sys.path.insert(0, '../route_calculator/')
 import db_util
 from db_util import *
-import cPickle
+import json
 import os
 import threading
 from threading import *
@@ -27,20 +27,40 @@ def get_all_routes():
     return routes
 
 def get_live_data(ts_api, route_names):
+	#start another thread to get data
+	t = Timer(10.0, get_live_data, args=(ts_api, route_names,))
+	t.start()
 
+	#get data
 	now = datetime.datetime.now().strftime("%y-%m-%d-%H-%M-%S")
+	data = {}
 
+	file_name = DATA_DIR + '/bus.json'
+
+	if os.path.exists(file_name):
+		with open(file_name) as f:
+			data = json.load(f) # dict {datetime, dict {route name, [bus instance data]}}
+			f.close()
+
+	route_dict = {}
 	for route_name in route_names:
+
 		route_json = ts_api.get_route_data(route_name)
 	
 		if len(route_json) > 0:
+			route_dict[route_name] = route_json
 
-			print route_name
+	data[now] = route_dict	
+		
+	with open(file_name, 'w') as f:	
+		f.write(json.dumps(data))
+		f.close()
 
-			file_name = DATA_DIR + '/' + route_name + '-' + now + '.txt'
-			f = open(file_name, 'wb')
-			cPickle.dump(route_json, f)
-			f.close()
+	str_result = "Successfully updated %s; Number of routes: %s" %(now, len(route_dict))
+	with open(DATA_DIR + '/log.txt', 'a') as log_file:
+		log_file.write(str_result + '\n')
+	print str_result
+
 
 if __name__ == '__main__':
 	ts_api = TransportationServiceAPI()
@@ -49,8 +69,12 @@ if __name__ == '__main__':
 	if not os.path.exists(DATA_DIR):
 		os.makedirs(DATA_DIR)
 
-	t = Timer(10.0, get_live_data, args=(ts_api, route_names,))
-	t.start()
+	get_live_data(ts_api, route_names)
+
+	# while True:
+	# 	get_live_data(ts_api, route_names)
+	# 	time.sleep(10)
+
 
 	
 
